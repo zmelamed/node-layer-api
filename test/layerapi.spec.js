7b59e19ee160e87c27100fae264dbc1bbb648694
Nil Gradisnik <nil.gradisnik@gmail.com>
2015-07-24 15:01:24 -0700

    Test cleanup

--- test/layerapi.spec.js
+++ test/layerapi.spec.js
@@ -8,24 +8,12 @@ var fixtures = require('./fixtures.json');
 var utils = require('../lib/utils');
 var LayerAPI = require('../lib');
 
-var RESOURCES = ['conversations', 'messages', 'announcements'];
-
 describe('Layer API constructor', function() {
-
-  describe('Passing config with full appId', function() {
-    it('should throw an error', function() {
-      var layerApi = new LayerAPI({token: fixtures.token, appId: fixtures.appIdFull});
-      should.exist(layerApi);
-    });
-  });
 
   describe('Passing config with token and appId', function() {
     it('should expose API operations', function() {
       var layerApi = new LayerAPI({token: fixtures.token, appId: fixtures.appId});
-
-      RESOURCES.forEach(function(resource) {
-        should.exist(layerApi[resource]);
-      });
+      should.exist(layerApi);
 
       should(typeof layerApi.conversations.create).be.eql('function');
       should(typeof layerApi.conversations.get).be.eql('function');
@@ -36,6 +24,13 @@ describe('Layer API constructor', function() {
       should(typeof layerApi.messages.sendTexFromUser).be.eql('function');
 
       should(typeof layerApi.announcements.send).be.eql('function');
+    });
+  });
+
+  describe('Passing config with token and full appId', function() {
+    it('should not throw an error', function() {
+      var layerApi = new LayerAPI({token: fixtures.token, appId: fixtures.appIdFull});
+      should.exist(layerApi);
     });
   });
 
